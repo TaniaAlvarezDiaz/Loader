94163f5146adebbe330ddc5ec4fe64f0360a96c7
Tania Alvarez Diaz <35775617+TaniaAlvarezDiaz@users.noreply.github.com>
2018-06-11 16:21:43 +0200

    Update User.java

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -9,7 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Users")
+@Table(name = "User")
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
